e2abf08d945140294e96b49b4763d63a888107be
Suhail <suhailcollegemail@gmail.com>
2024-11-24 10:46:14 +0530

    done 99%

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <section className="max-w-7xl mx-auto px-4 md:px-8 mb-24">
       <div className="max-w-3xl mx-auto text-2xl sm:text-5xl lg:text-6xl font-semibold text-center">
-        <h1>Find the best Tailwind</h1>
+        <h1>Find the best </h1>
         <h1 className="text-primary">Templates & Icons</h1>
         <p className="lg:text-lg text-muted-foreground mx-auto mt-5 w-[90%] font-normal text-base">
-          MarshalUi stands out as the premier marketplace for all things related
+          LiahusUI stands out as the premier marketplace for all things related
           to tailwindcss, offering an unparalleled platform for both sellers and
           buyers alike.
         </p>
